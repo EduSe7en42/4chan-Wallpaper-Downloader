67c57b3abdf0bbe8cee9efa880eb0b159e0b217f
AppleTheBloom <dneemail@dnmx.org>
2023-10-18 03:35:14 -0300

    Alterações para sistemas linux

--- Download-Images.ps1
+++ Download-Images.ps1
@@ -1,20 +1,27 @@
 function Download-Images 
 {
     param([string[]] $urlImages)
+    $env = [Environment]::OSVersion.VersionString
 
     $counter = 0
 
-    if (-not (Test-Path -Path "./images/"))
+    If (-not (Test-Path -Path "./images/"))
     {
         New-Item -ItemType Directory -Force -Path "./4chan_images/"
     }
 
-    foreach ($url in $urlImages) 
+    ForEach ($url in $urlImages) 
     {
         $counter++
-        
-        $newUrl = $url.replace("about://", "http://")
+
+        if ($env -contains "Windows") {
+            $newUrl = $url.replace("about://", "https://")
+        } else {
+            $newUrl = $url.replace("//", "https://")
+        }
+
         $filePath = "./4chan_images/image_" + $counter.ToString() + ".jpg"
         Invoke-WebRequest $newUrl -OutFile $filePath
+        Write-Host "File saved with success in " + $filePath 
     }
 }
